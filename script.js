751efbcdd8d97205e8104b4cb6bd86f98d21aafa
Amanda Papalardo <136401392+amandapapalardo@users.noreply.github.com>
2025-06-16 10:44:21 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("btnClique").addEventListener("click", function() {
+  const resposta = document.getElementById("resposta");
+  resposta.textContent = "Você clicou no botão! 🎉";
+});
